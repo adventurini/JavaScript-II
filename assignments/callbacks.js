10554c66f1f3cd818dbdf05d3178281c17650039
Anthony Venturini <anthonydventurini@gmail.com>
2019-01-22 16:07:17 -0800

    MVP Met

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,27 +21,59 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+function firstItem(arr, cb) {
+  return cb(arr[0]);
+}
+
+firstItem(items, function(first) {
+  console.log(first)
+});
+
+
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+return cb(arr.length);
 }
+
+getLength(items, function(len) {
+  console.log(len);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-}
+    return cb(arr[3]);
+  }
+  
+  last(items, function(last) {
+    console.log(last)
+  });
+
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);// sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+
+sumNums(3, 4, function(sum){
+  console.log(sum);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
+multiplyNums(2, 4, function(multiply){
+  console.log(multiply);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
+
+contains('lips', ['lips', 'mouth', 'tooth'], function(con){
+     console.log(con);
+})
 
 /* STRETCH PROBLEM */
 
@@ -49,4 +81,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+//   var newArray=[];
+// cb(array.filter((item, index)=> 
+// }
+
+// removeDuplicates([1,2,3,7,3], function(nm){
+//   console.log(nm);
+// 
 }
